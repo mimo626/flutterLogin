4f9c97447acb13f320ff720f1f7108d0a3366d7e
mimo626 <yessuk5499@gmail.com>
2024-09-09 16:16:11 +0900

    파일 추가

---
+++ lib/features/login/domain/entity/user.dart
@@ -0,0 +1,12 @@
+class User {
+  final String email;
+  final String? password;
+  final String? nickname;
+
+  User(
+    {
+    required this.email,
+      this.password,
+      this.nickname,
+  });
+}
