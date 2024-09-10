d3a019351f49042e97e9ce6fe7cc23a1ad365ecf
mimo626 <yessuk5499@gmail.com>
2024-09-10 16:44:46 +0900

    회원가입 엔티티 닉네임도 추가

--- lib/features/signup/domain/usecases/user_usecase.dart
+++ lib/features/signup/domain/usecases/user_usecase.dart
@@ -8,7 +8,7 @@ class UserUsecase {
   UserUsecase(this.repository);
 
   Future<void> execute(String email, String password, String nickname) async {
-    final user = SignupUser(email: email, password: password);
+    final user = SignupUser(email: email, password: password, nickname: nickname);
     await repository.signUp(user);
   }
 
