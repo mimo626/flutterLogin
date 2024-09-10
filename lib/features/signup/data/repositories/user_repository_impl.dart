4a1305f3ee61aa3725327549cae6195c1074bbf2
mimo626 <yessuk5499@gmail.com>
2024-09-10 16:45:19 +0900

    회원가입 datasource에서 진행하도록 수정

--- lib/features/signup/data/repositories/user_repository_impl.dart
+++ lib/features/signup/data/repositories/user_repository_impl.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:login/features/signup/data/datasources/user_datasource.dart';
+import 'package:login/features/signup/data/mapper/signup_user_mapper.dart';
+import 'package:login/features/signup/data/model/signup_user_model.dart'; // 파일명 오타 수정
 import 'package:login/features/signup/domain/entity/signup_user.dart';
 import 'package:login/features/signup/domain/repositories/user_repository.dart';
-
 
 class UserRepositoryImpl implements UserRepository {
   final firebase_auth.FirebaseAuth _firebaseAuth;
@@ -13,18 +15,17 @@ class UserRepositoryImpl implements UserRepository {
   @override
   Future<void> signUp(SignupUser user) async {
     try {
-      final credentials = await _firebaseAuth.createUserWithEmailAndPassword(
-        email: user.email,
-        password: user.password!!,
-      );
+      // UserDatasource 인스턴스 생성
+      final userDatasource = UserDatasource(_firebaseAuth, _firestore);
 
-      await _firestore.collection('users').doc(credentials.user!.uid).set({
-        'email': user.email,
-        'password': user.password,
-        'nickname': user.nickname,
-      });
+      // 도메인 엔티티 -> 데이터 모델로 변환
+      SignupUserModel signupUserModel = SignupUserMapper.entityToModel(user);
+
+      // UserDatasource의 signUp 메서드 호출
+      await userDatasource.signUp(signupUserModel);
     } catch (e) {
-      throw Exception('Failed to sign up: $e');
+      print('Error during signup: $e');
+      throw Exception('Failed to sign up: ${e.toString()}');
     }
   }
 }
